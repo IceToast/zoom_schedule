0763450d7125e35ed7a3a8f19c82d18763b942ea
IceToast <54889359+IceToast@users.noreply.github.com>
2021-03-25 23:45:58 +0100

    Changed Login Form to OAuth Provider Buttons

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,37 +1,32 @@
 import React from 'react';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import {
-  Avatar,
-  Button,
-  CssBaseline,
-  TextField,
-  FormControlLabel,
-  Checkbox,
-  Link,
-  Grid,
-  Box,
-  Typography,
-  Container,
-} from '@material-ui/core';
+import {Avatar, Button, Box, Typography, Grid} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 
 const useStyles = makeStyles(theme => ({
   paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
+    minHeight: '80vh',
     alignItems: 'center',
+    justifyContent: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
+  loginButtons: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: theme.spacing(2),
+    '& Button': {
+      margin: theme.spacing(1),
+      height: '3em',
+      border: 'solid #c9c9c9 0.5px',
+    },
+    '& img': {
+      marginRight: theme.spacing(1),
+      width: '2em',
+      height: '2em',
+    },
   },
 }));
 
@@ -39,61 +34,28 @@ const SignIn = () => {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-        <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}>
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item>
-              <Link href="#" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}></Box>
-    </Container>
+    <Grid container direction="column" className={classes.paper}>
+      <Avatar className={classes.avatar}>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+        Sign in
+      </Typography>
+      <Box className={classes.loginButtons}>
+        <Button id="google">
+          <img alt="Sign In" src="/images/loginButtons/google.svg" />
+          with Google
+        </Button>
+        <Button id="discord">
+          <img alt="Sign In" src="/images/loginButtons/discord.svg" />
+          with Discord
+        </Button>
+        <Button id="Github">
+          <img alt="Sign In" src="/images/loginButtons/github.svg" />
+          with Github
+        </Button>
+      </Box>
+    </Grid>
   );
 };
 
