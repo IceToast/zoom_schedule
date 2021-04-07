64a30d6bc2d44a8b3f0eeae0863e3d0ede5cece8
IceToast <54889359+IceToast@users.noreply.github.com>
2021-04-07 15:04:07 +0200

    Fix useEffekt Warning

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,35 +1,34 @@
 import React, {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
-import { loadMeetings } from '../actions/actions.loadMeetings';
-import { LOAD_MEETINGS } from '../actions/actions.loadMeetings';
+import {loadMeetings} from '../actions/actions.loadMeetings';
+import {LOAD_MEETINGS} from '../actions/actions.loadMeetings';
 
 import {DaysContainer} from '.';
-import { Query } from '@redux-requests/react';
+import {Query} from '@redux-requests/react';
 
 const RequestError = () => (
   <p>There was some error during fetching. Please try again.</p>
 );
 
 const Schedule = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadMeetings())
-  }, []); 
+    dispatch(loadMeetings());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <Query
       type={LOAD_MEETINGS}
       errorComponent={RequestError}
       // loadingComponent={Spinner}
-      noDataMessage={<p>There is no entity currently.</p>}
-    >
-      {({ data }) => {
+      noDataMessage={<p>There is no entity currently.</p>}>
+      {({data}) => {
         console.log(data);
-        return <DaysContainer days={data.week.days} />
+        return <DaysContainer days={data.week.days} />;
       }}
     </Query>
-  ) ;
+  );
 };
 export default Schedule;
