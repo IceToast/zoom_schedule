467c7e9ad5f9aba079f746cda88c82f34e7efda9
IceToast <54889359+IceToast@users.noreply.github.com>
2021-04-11 00:22:53 +0200

    Removed comments

--- src/actions/actions.meeting.js
+++ src/actions/actions.meeting.js
@@ -1,21 +1,10 @@
-import {createAction} from '@reduxjs/toolkit';
+import { createAction } from '@reduxjs/toolkit';
 
 export const fetchMeetings = createAction('fetchMeetings', _ => ({
   payload: {
-    request: {url: '/meeting', method: 'GET'},
+    request: { url: '/meeting', method: 'GET' },
   },
 }));
-
-//      mutations: {
-//        [fetchMeetings]: (data, mutationData) => {
-//          const dayIndex = data.findIndex(day => day.name === postData.day);
-//          const newData = [...data];
-//          if (newData[dayIndex]) {
-//            newData[dayIndex].meetings = newData[dayIndex].meetings.concat(
-//              mutationData
-//            );
-//          }
-//          return newData;
 
 export const createMeeting = createAction('createMeeting', meetingData => ({
   payload: {
@@ -32,15 +21,6 @@ export const createMeeting = createAction('createMeeting', meetingData => ({
   },
 }));
 
-//      mutations: {
-//        [fetchMeetings]: data => {
-//          const dayIndex = data.findIndex(day => day.name === deleteDay);
-//          const newData = [...data];
-//          if (newData[dayIndex]) {
-//            newData[dayIndex].meetings = newData[dayIndex].meetings.filter(meeting => meeting._id !== deleteMeeting._id);
-//          }
-//          return newData;
-
 export const deleteMeeting = createAction('deleteMeeting', (deleteMeeting, deleteDay) => ({
   payload: {
     request: {
@@ -53,16 +33,6 @@ export const deleteMeeting = createAction('deleteMeeting', (deleteMeeting, delet
     },
   },
 }));
-
-//      mutations: {
-//        [fetchMeetings]: data => {
-//          const dayIndex = data.findIndex(day => day.name === putData.day);
-//          const newData = [...data];
-//          if (newData[dayIndex]) {
-//            const meetingIndex = newData[dayIndex].meetings.findIndex(meeting => meeting._id === putData._id);
-//            newData[dayIndex].meetings[meetingIndex] = putData;
-//          }
-//          return newData;
 
 export const editMeeting = createAction('editMeeting', meetingData => ({
   payload: {
