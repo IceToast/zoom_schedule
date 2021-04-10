634f648960b6a61f4a6b5c7b510184a72c19e6d6
IceToast <54889359+IceToast@users.noreply.github.com>
2021-04-10 21:31:48 +0200

    Fixed dispatches (temp)

--- src/actions/actions.deleteMeeting.js
+++ src/actions/actions.deleteMeeting.js
@@ -1,29 +1,30 @@
-import { LOAD_MEETINGS } from './actions.loadMeetings';
+import {fetchMeetings} from './actions.loadMeetings';
 export const DELETE_MEETING = 'DELETE_MEETING';
 
 export const deleteMeeting = (deleteMeeting, deleteDay) => {
-
-    return {
-        type: DELETE_MEETING,
-        request: {
-            url: '/meeting',
-            method: 'DELETE',
-            data: {
-                id: deleteMeeting._id,
-                day: deleteDay
-            },
+  return {
+    type: DELETE_MEETING,
+    request: {
+      url: '/meeting',
+      method: 'DELETE',
+      data: {
+        id: deleteMeeting._id,
+        day: deleteDay,
+      },
+    },
+    meta: {
+      mutations: {
+        [fetchMeetings]: data => {
+          const dayIndex = data.findIndex(day => day.name === deleteDay);
+          const newData = [...data];
+          if (newData[dayIndex]) {
+            newData[dayIndex].meetings = newData[dayIndex].meetings.filter(
+              meeting => meeting._id !== deleteMeeting._id
+            );
+          }
+          return newData;
         },
-        meta: {
-            mutations: {
-                [LOAD_MEETINGS]: data => {
-                    const dayIndex = data.findIndex(day => day.name === deleteDay);
-                    const newData = [...data]
-                    if(newData[dayIndex]){
-                        newData[dayIndex].meetings = newData[dayIndex].meetings.filter(meeting => meeting._id !== deleteMeeting._id)
-                    }
-                    return newData;
-                }
-            }
-        }
-    }
-}
+      },
+    },
+  };
+};
--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,16 +1,17 @@
 import React, {useEffect} from 'react';
 import {useDispatch} from 'react-redux';
-import {loadMeetings} from '../actions/actions.loadMeetings';
-import {LOAD_MEETINGS} from '../actions/actions.loadMeetings';
-import { setLoggedInState } from '../actions/actions.setLoggedInState';
+import {fetchMeetings} from '../actions/actions.loadMeetings';
+import {setLoggedInState} from '../actions/actions.setLoggedInState';
 
 import {DaysContainer} from '.';
 import {Query} from '@redux-requests/react';
-import { Container, Typography } from '@material-ui/core';
+import {Container, Typography} from '@material-ui/core';
 
 const RequestError = () => (
   <Container>
-    <Typography>Ein Fehler ist aufgetaucht. Bitte versuche es erneut.</Typography>
+    <Typography>
+      Ein Fehler ist aufgetaucht. Bitte versuche es erneut.
+    </Typography>
   </Container>
 );
 
@@ -18,33 +19,94 @@ const Spinner = () => (
   <Container>
     <Typography>Lädt...</Typography>
   </Container>
-)
+);
 
 const Schedule = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(loadMeetings()).then(({error}) => {
+    dispatch(fetchMeetings()).then(({error}) => {
       const errResStatus = error?.response?.status;
       const errorMessage = error?.response?.data;
 
-      if(errResStatus === 403 && errorMessage === 'invalid Cookie or session expired' && process.env.NODE_ENV !== 'development'){
+      if (
+        errResStatus === 403 &&
+        errorMessage === 'invalid Cookie or session expired' &&
+        process.env.NODE_ENV !== 'development'
+      ) {
         dispatch(setLoggedInState(false));
-      }else{
+      } else {
         dispatch(setLoggedInState(true));
       }
     });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  
-  // use dummy data in development mode 
-  if(process.env.NODE_ENV === 'development'){
-    return <DaysContainer days={[{"name":"Monday","meetings":[{"_id":"606ef8bc7fe20230958101d7","name":"New Meeting","link":"https://randomlink.com","password":"Password"}]},{"name":"Tuesday","meetings":[{"_id":"606ef8c27fe20230958101da","name":"New Meeting","link":"https://randomlink.com","password":"Password"}]},{"name":"Wedensday"},{"name":"Thursday","meetings":[{"_id":"606ef8c77fe20230958101dd","name":"New Meeting","link":"https://randomlink.com","password":"Password"}]},{"name":"Friday","meetings":[{"_id":"606ef8cb7fe20230958101e0","name":"New Meeting","link":"https://randomlink.com","password":"Password"},{"_id":"606ef8cb7fe20230958101e3","name":"New Meeting","link":"https://randomlink.com","password":"Password"}]},{"name":"Saturday"}]} />;
+
+  // use dummy data in development mode
+  if (process.env.NODE_ENV === 'development') {
+    return (
+      <DaysContainer
+        days={[
+          {
+            name: 'Monday',
+            meetings: [
+              {
+                _id: '606ef8bc7fe20230958101d7',
+                name: 'New Meeting',
+                link: 'https://randomlink.com',
+                password: 'Password',
+              },
+            ],
+          },
+          {
+            name: 'Tuesday',
+            meetings: [
+              {
+                _id: '606ef8c27fe20230958101da',
+                name: 'New Meeting',
+                link: 'https://randomlink.com',
+                password: 'Password',
+              },
+            ],
+          },
+          {name: 'Wedensday'},
+          {
+            name: 'Thursday',
+            meetings: [
+              {
+                _id: '606ef8c77fe20230958101dd',
+                name: 'New Meeting',
+                link: 'https://randomlink.com',
+                password: 'Password',
+              },
+            ],
+          },
+          {
+            name: 'Friday',
+            meetings: [
+              {
+                _id: '606ef8cb7fe20230958101e0',
+                name: 'New Meeting',
+                link: 'https://randomlink.com',
+                password: 'Password',
+              },
+              {
+                _id: '606ef8cb7fe20230958101e3',
+                name: 'New Meeting',
+                link: 'https://randomlink.com',
+                password: 'Password',
+              },
+            ],
+          },
+          {name: 'Saturday'},
+        ]}
+      />
+    );
   }
 
   return (
     <Query
-      type={LOAD_MEETINGS}
+      type={'fetchMeetings'}
       errorComponent={RequestError}
       loadingComponent={Spinner}
       noDataMessage={<p>There is no entity currently.</p>}>
