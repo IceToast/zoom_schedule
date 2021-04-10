926ba8bbd6d0c8481fddc03b288bbd9d16dc3259
IceToast <54889359+IceToast@users.noreply.github.com>
2021-04-11 01:47:05 +0200

    Fix PWA

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import withReduxFeatures from './withReduxFeatures';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 /** Wrap App component with store providers */
 const WrappedApp = withReduxFeatures(App);
 
 ReactDOM.render(<WrappedApp />, document.getElementById('root'));
 
-/**
- * If you want your app to work offline and load faster,
- * you can change unregister() to register() below.
- * Note this comes with some pitfalls.
- * @see https://bit.ly/CRA-PWA
- */
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
