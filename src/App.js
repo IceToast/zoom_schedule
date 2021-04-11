198f33b5fc0b935b7590fabe7ca8f98b88a46ef4
Nihad Amin <neihed@hotmail.de>
2021-04-11 23:45:04 +0200

    implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { AppBar, Container, IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
+import { AppBar, Avatar, Container, IconButton, Menu, MenuItem, Typography } from '@material-ui/core';
 import { Schedule, SignIn } from './components';
 import { useDispatch, useSelector } from 'react-redux';
 import { setThemeState } from './actions/actions.setThemeState';
 import { fetchMeetings } from './actions/actions.meeting';
 import { setLoginState } from './actions/actions.auth';
 import { AccountCircle as AccountCircleIcon, Brightness7 as DarkIcon, Brightness4 as BrightIcon } from '@material-ui/icons';
+import axios from 'axios';
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -42,6 +43,8 @@ const App = () => {
   const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
   const themePaletteType = useSelector(state => state.theme.paletteType);
   const [dropdownAnchorEl, setDropdownAnchorEl] = useState(null);
+  const [user, setUser] = useState();
+
   const classes = useStyles({ themePaletteType });
   const dispatch = useDispatch();
 
@@ -61,8 +64,19 @@ const App = () => {
         dispatch(setLoginState(true));
       }
     });
+
+    loadUser();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  async function loadUser() {
+    try {
+      const { data: user } = await axios.get('/api/user');
+      setUser(user);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 
   function handleDarkModeButtonClick() {
     if (themePaletteType === 'dark') {
@@ -108,27 +122,42 @@ const App = () => {
                   <BrightIcon className={classes.darkModeIcons} />
                 )}
               </IconButton>
-              <IconButton
-                className={classes.userButton}
-                onClick={openUserDropdownMenu}
-                color="secondary.main"
-                variant="contained">
-                <AccountCircleIcon className={classes.avatarIcon} />
-              </IconButton>
-              <Menu
-                id="user-menu"
-                anchorEl={dropdownAnchorEl}
-                keepMounted
-                open={Boolean(dropdownAnchorEl)}
-                onClose={closeUserDropdownMenu}>
-                <MenuItem disabled>Max Mustermann</MenuItem>
-                <MenuItem onClick={() => pushToPathname('/api/user/logout')} className={classes.logoutButton}>
-                  Logout
-                </MenuItem>
-                <MenuItem disabled onClick={() => pushToPathname('/api/user/logoutall')} className={classes.logoutButton}>
-                  Logout from all devices
-                </MenuItem>
-              </Menu>
+              {user ? (
+                <>
+                  <IconButton
+                    className={classes.userButton}
+                    onClick={openUserDropdownMenu}
+                    color="secondary.main"
+                    variant="contained">
+                    <Avatar className={classes.avatarIcon} alt={user.username} src={user.avatarurl} />
+                  </IconButton>
+                  <Menu
+                    id="user-menu"
+                    anchorEl={dropdownAnchorEl}
+                    keepMounted
+                    open={Boolean(dropdownAnchorEl)}
+                    onClose={closeUserDropdownMenu}>
+                    <MenuItem disabled>{user.username}</MenuItem>
+                    <MenuItem onClick={() => pushToPathname('/api/user/logout')} className={classes.logoutButton}>
+                      Logout
+                    </MenuItem>
+                    <MenuItem
+                      disabled
+                      onClick={() => pushToPathname('/api/user/logoutall')}
+                      className={classes.logoutButton}>
+                      Logout from all devices
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <IconButton
+                  className={classes.userButton}
+                  onClick={openUserDropdownMenu}
+                  color="secondary.main"
+                  variant="contained">
+                  <AccountCircleIcon className={classes.avatarIcon} />
+                </IconButton>
+              )}
             </div>
           </Container>
         </AppBar>
